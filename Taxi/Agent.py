0ff681c4815eb270fc2d8f960b4654fafe0a8759
Joaquin Rodriguez <joaquinrodriguezcaussi@gmail.com>
2024-06-04 16:43:00 -0300

    Major changes

--- Taxi/Agent.py
+++ Taxi/Agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import wandb
+from tqdm import tqdm
 
 class Agent():
     def __init__(self, env, Qtable, gamma, alpha, epsilon, randomSeed, policy_func):
@@ -18,9 +19,9 @@ class Agent():
         return self.policy_func(state, self.Qtable, self.epsilon, self.randomSeed)
         
     def train(self, num_k_episodes=1000):
-        for i in range(num_k_episodes):
+        all_rewards = []
+        for i in tqdm(range(num_k_episodes)):
             obs,_ = self.env.reset()
-            print(obs)
             done = False
             total_reward = 0
             step_count = 0
@@ -31,13 +32,14 @@ class Agent():
                 self.Qtable[state, action] += self.alpha * (reward + self.gamma * np.max(self.Qtable[obs]) - self.Qtable[state, action]) ##Q-Learning
                 total_reward += reward
                 step_count += 1
-                print('->', state, action, reward, obs, done)
                 self.env.render()
-            print("Episode finished: reward={0}, steps={1}".format(total_reward, step_count))
-            if i % 2 == 0:
-                wandb.log({'reward': total_reward, 'episode' : i, "Qtable": self.Qtable})
+            all_rewards.append(total_reward)
+            if i % 10 == 0:
+                wandb.log({'reward': total_reward, 'episode' : i, "Qtable": self.Qtable })
+        return np.mean(all_rewards)
                 
     def play(self, num_l_episodes=100):
+        all_rewards = []
         for _ in range(num_l_episodes):
             obs,_ = self.env.reset()
             total_reward = 0
@@ -50,4 +52,5 @@ class Agent():
                 total_reward += reward
                 step_count += 1
                 self.env.render()
-            print("Episode finished: reward={0}, steps={1}".format(total_reward, step_count))
+            all_rewards.append(total_reward)
+        return np.mean(all_rewards) 
