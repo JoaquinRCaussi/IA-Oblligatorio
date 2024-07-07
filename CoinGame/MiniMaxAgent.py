40cf9737c774b5592700bcac829131fef1a8e69a
Joaquin Rodriguez <joaquinrodriguezcaussi@gmail.com>
2024-07-06 21:19:41 -0300

    Final commit

--- CoinGame/MiniMaxAgent.py
+++ CoinGame/MiniMaxAgent.py
@@ -6,16 +6,29 @@ class MiniMaxAgent(Agent):
     def __init__(self, player=1, MaxDepth=1):
         super().__init__(player)
         self.Maxdepth = MaxDepth
-        
+
+    def eval_disparity(board):
+        row_lengths = [len(row) for row in board]
+        return -(max(row_lengths) - min(row_lengths))
+    
+    def eval_max_coins_in_row(board):
+        return max(len(row) for row in board)
+    
+    def eval_QuantityofCoins(board):
+        return sum(len(row) for row in board)
+    
+    def eval_possible_moves(board):
+        return len(board.get_possible_actions())
+
     def next_action(self, obs):
-        _, best_action= self.minimax(obs, 0 , self.player)
+        _, best_action= self.minimax(obs, 0 , self.player, float('-inf'), float('inf')) # inicializamos la beta en infinito y el alpha en -infinito
         return best_action
     
     def heuristic_utility(self, board):
-        total_coins = np.sum(board.grid == 1)  # Contar monedas restantes
-        return -total_coins  # Menos monedas restantes es mejor
+         # Contar monedas restantes
+        return eval_disparity(board) # Menos monedas restantes es mejor
     
-    def minimax(self, board, depth, MaxPlayer):
+    def minimax(self, board, depth, MaxPlayer, alpha, beta):
         if board.is_end(self.player)[0] or depth == self.Maxdepth:
             return self.heuristic_utility(board), None  # Devolver None para la acción en el caso base
     
@@ -27,10 +40,13 @@ class MiniMaxAgent(Agent):
             for action in actions:
                 board_copy = board.clone()
                 board_copy.play(action)
-                value, _ = self.minimax(board_copy, depth + 1,3 -self.player)  # Le damos el turno al otro jugador
+                value, _ = self.minimax(board_copy, depth + 1,3 -self.player, alpha, beta)  # Le damos el turno al otro jugador
                 if value > best_value:
                     best_value = value
                     best_action = action
+                alpha = max(alpha, best_value)
+                if beta <= alpha:
+                    break  # Cortar la rama si beta es menor o igual a alpha
             return best_value, best_action  # Devolver tanto el valor como la acción
 
         else:  # Minimizar la utilidad
@@ -39,8 +55,11 @@ class MiniMaxAgent(Agent):
             for action in actions:
                 board_copy = board.clone()
                 board_copy.play(action)
-                value, _ = self.minimax(board_copy, depth + 1,3 -self.player) # Le damos el turno al otro jugador
+                value, _ = self.minimax(board_copy, depth + 1,3 -self.player, alpha, beta) # Le damos el turno al otro jugador
                 if value < best_value:
                     best_value = value
                     best_action = action
+                beta = min(beta, best_value)
+                if beta <= alpha:
+                    break  # Cortar la rama si beta es menor o igual a alpha
             return best_value, best_action
